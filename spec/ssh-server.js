ca4ce41ceb66d5195d590d32af86cdb36fd552d9
Steel Brain <i@steelbrain.me>
2016-08-21 22:20:09 -0700

    :new: Add Test server to test module with

---
+++ spec/ssh-server.js
@@ -0,0 +1,144 @@
+/* @flow */
+
+import FS from 'fs'
+import { flagsToString } from 'ssh2-streams'
+import ChildProcess from 'child_process'
+
+import pty from 'pty.js'
+import ssh2 from 'ssh2'
+import { PRIVATE_KEY_PATH } from './helpers'
+
+const STATUS_CODE = ssh2.SFTP_STATUS_CODE
+
+function handleSFTP(accept) {
+  const sftpStream = accept()
+  const handles: Set<number> = new Set()
+  const filesMap: Map<number, string> = new Map()
+  sftpStream.on('OPEN', function(reqid, filename, flags) {
+    let handleId
+    try {
+      handleId = FS.openSync(filename, flagsToString(flags))
+    } catch (error) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+      return
+    }
+    handles.add(handleId)
+    filesMap.set(handleId, filename)
+
+    const handle = new Buffer(4)
+    handle.writeUInt32BE(handleId, 0, true)
+    sftpStream.handle(reqid, handle)
+  })
+  sftpStream.on('READ', function(reqid, givenHandle, offset, length) {
+    const handle = parseInt(givenHandle, 10)
+    if (!handles.has(handle)) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+      return
+    }
+
+    const contents = new Buffer(length)
+    try {
+      FS.readSync(handle, contents, 0, length, offset)
+    } catch (error) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+      return
+    }
+    sftpStream.data(reqid, contents)
+  })
+  sftpStream.on('WRITE', function(reqid, givenHandle, offset, data) {
+    const handle = parseInt(givenHandle, 10)
+    if (!handles.has(handle)) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+      return
+    }
+
+    try {
+      FS.writeSync(handle, data, 0, data.length, offset)
+      sftpStream.status(reqid, STATUS_CODE.OK)
+    } catch (error) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+    }
+  })
+  sftpStream.on('FSTAT', function(reqid, givenHandle) {
+    const handle = parseInt(givenHandle, 10)
+    if (!handles.has(handle)) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+      return
+    }
+
+    let stats
+    try {
+      stats = FS.statSync(filesMap.get(handle) || '')
+    } catch (error) {
+      sftpStream.status(reqid, STATUS_CODE.FAILURE)
+      return
+    }
+    sftpStream.attrs(reqid, stats)
+  })
+  sftpStream.on('CLOSE', function(reqid, givenHandle) {
+    const handle = parseInt(givenHandle, 10)
+    if (handles.has(handle)) {
+      handles.delete(handle)
+      filesMap.delete(handle)
+      FS.close(handle)
+      sftpStream.status(reqid, STATUS_CODE.OK)
+    } else {
+      sftpStream.status(reqid, STATUS_CODE.FALIURE)
+    }
+  })
+}
+
+function handleSession(acceptSession) {
+  const session = acceptSession()
+  let ptyInfo: ?Object = null
+  session.on('pty', function(accept, _, info) {
+    accept()
+    ptyInfo = {
+      name: info.term,
+      cols: info.cols,
+      rows: info.rows,
+      cwd: process.env.HOME,
+      env: process.env,
+    }
+  })
+  session.on('shell', function(accept, reject) {
+    if (!ptyInfo) {
+      reject()
+      return
+    }
+    const request = accept()
+    const spawnedProcess = pty.spawn(process.env.SHELL || 'bash', [], ptyInfo)
+    request.pipe(spawnedProcess.socket)
+    spawnedProcess.stdout.pipe(request)
+  })
+  session.on('exec', function(accept, reject, info) {
+    const response = accept()
+    const spawnedProcess = ChildProcess.exec(info.command)
+    response.pipe(spawnedProcess.stdin)
+    spawnedProcess.stdout.pipe(response)
+    spawnedProcess.stderr.pipe(response.stderr)
+  })
+  session.on('sftp', handleSFTP)
+}
+
+function handleAuthentication(ctx) {
+  let accept = true
+  if (ctx.method === 'password') {
+    accept = ctx.username === 'steel' && ctx.password === 'password'
+  }
+  if (accept) {
+    ctx.accept()
+  } else {
+    ctx.reject()
+  }
+}
+
+export default function createServer() {
+  const server = new ssh2.Server({
+    hostKeys: [FS.readFileSync(PRIVATE_KEY_PATH)],
+  }, function(client) {
+    client.on('authentication', handleAuthentication)
+    client.on('session', handleSession)
+  })
+  return server
+}
